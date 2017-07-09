8d824c0f6242f94e6ff5bf163c77a210d805a453
Romario Raffington <romarioraff@gmail.com>
2017-07-09 17:48:46 -0500

    Created checkAvatarURL to add a default avatarURL if one is not provided

--- src/modules/controller.ts
+++ src/modules/controller.ts
@@ -1,5 +1,6 @@
 import * as clone from 'clone'
 import Firebase from './firebase';
+import { config } from '../config';
 import { contact } from './interface';
 
 export default class Controller {
@@ -17,14 +18,24 @@ export default class Controller {
     if (!contact.id) {
       contact.id = Math.random().toString(36).substr(-8)
     }
-    this.firebase.create(contact);
+    this.firebase.create(this.checkAvatarUrl(contact));
   }
 
   public update(id: string, contact: contact): void {
-    this.firebase.update(id, contact);
+    this.firebase.update(id, this.checkAvatarUrl(contact));
   }
 
   public delete(id: string): void {
     this.firebase.delete(id);
+  }
+
+  private checkAvatarUrl(contact: contact): contact {
+    const avatars = ['bulbasaur.png', 'charmander.png', 'pikachu.png'];
+    const randomNumber = Math.floor(Math.random() * 3)
+
+    if (!contact.avatarUrl) {
+      contact.avatarUrl = `${config.origin}/${avatars[randomNumber]}`;
+    }
+    return contact;
   }
 }
