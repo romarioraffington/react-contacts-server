cc0cc999df96b47684c09147f73a34389029784f
Romario Raffington <romarioraff@gmail.com>
2017-07-14 15:10:57 -0500

    Removed config script

--- gulpfile.js
+++ gulpfile.js
@@ -15,15 +15,9 @@ gulp.task('assets', function() {
   .pipe(gulp.dest('dist/public'));
 });
 
-// Key 
-gulp.task('config', function() {
-  return gulp.src('src/config/firebaseKey.json')
-  .pipe(gulp.dest('dist/config'));
-});
-
 gulp.task('watch', ['scripts'], () => {
   gulp.watch('src/**/*.ts', ['scripts']);
 });
 
 gulp.task('development', ['watch', 'assets']);
-gulp.task('default', ['scripts', 'assets', 'config']);
+gulp.task('default', ['scripts', 'assets']);
